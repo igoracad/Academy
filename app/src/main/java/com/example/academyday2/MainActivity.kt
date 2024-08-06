99a61d42f6486b533940897bf7e8c712ea7226eb
igoracad <igordev24@gmail.com>
2024-08-06 17:12:22 +0100

    jobscheduler

--- app/src/main/java/com/example/academyday2/MainActivity.kt
+++ app/src/main/java/com/example/academyday2/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.academyday2
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
 import android.content.ComponentName
 import android.content.Intent
 import android.content.ServiceConnection
@@ -20,6 +22,10 @@ import com.example.academyday2.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+
+    companion object{
+         var mScheduler: JobScheduler? = null
+    }
 
 
     var TAG = MainActivity::class.java.simpleName
@@ -56,6 +62,14 @@ class MainActivity : AppCompatActivity() {
         binding.btnBind.setOnClickListener{
             bindService(serviceIntent,mConnection, BIND_AUTO_CREATE)
         }
+
+        binding.btnJob.setOnClickListener {
+            scheduleJob()
+        }
+
+        binding.btnUnjob.setOnClickListener {
+            cancelJobs()
+        }
     }
 
     val mConnection = object : ServiceConnection {
@@ -74,6 +88,26 @@ class MainActivity : AppCompatActivity() {
         override fun onServiceDisconnected(name: ComponentName?) {
             Log.i(TAG,"service disconnected --")
 
+        }
+    }
+
+
+    fun scheduleJob() {
+        mScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+
+        val serviceName = ComponentName(packageName, MyService::class.java.name)
+        val builder = JobInfo.Builder(0, serviceName)
+            .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+
+        val myJobInfo = builder.build()
+        mScheduler!!.schedule(myJobInfo)
+    }
+
+    fun cancelJobs() {
+        if (mScheduler != null){
+            mScheduler!!.cancelAll()
+            mScheduler = null
+            //Toast.makeText(this, R.string.jobs_canceled, Toast.LENGTH_SHORT).show();
         }
     }
 
